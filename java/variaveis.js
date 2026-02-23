b0ebd91a6dbb26739c5d4ba9d7476ef2a369d170
Yasmin <yasmincarvalhobraga2@gmail.com>
2026-02-23 08:44:54 -0300

    Aula 01

---
+++ java/variaveis.js
@@ -0,0 +1,27 @@
+var nomedavariavel = "valor da variável";
+var outravariavel = 'valor da outra variável';
+var variavelIdade = 22;
+var variavelTemCabeloBranco = false;
+//var variavelIndefinida ;
+
+console.log(nomedavariavel);
+console.log(outravariavel);
+console.log(variavelIdade);
+console.log(variavelTemCabeloBranco);
+//console.log(variavelIndefinida);
+//console.log(variavelNula);
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+//Neste caso tr irá conter o valor true e fl o valor false.
+var int1 = tr + 1;
+var int2 = fl + 1;
+//A variável int1 irá conter o valor 2 (true + 1), pois o true é automaticamente convertido para 1 e int2 irá conter o valor 1 (false + 1), pois false é convertido para 0.
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
+console.log(int1);
+console.log(int2);
